3bd1a01feeac52ab6b17ec37d5727a05b7cefae0
scole261 <scole261@TVHOELTMSLV43>
2023-05-16 11:31:21 -0700

    Add project files.

---
+++ ScottSession2NYC_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ScottSession2NYC_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("ScottSession2NYC_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("166F3157-1539-40CA-8A93-6C715384906D")]
+[assembly: AssemblyVersion("1.0.0.0")]
